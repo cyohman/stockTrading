422c92fee32eef49dd8f312c92feb43f4be32f6d
Chance Yohman <chance.yohman@gmail.com>
2020-04-30 19:30:27 -0400

    Closes #5

--- stockTrading.py
+++ stockTrading.py
@@ -1,24 +1,27 @@
 import os
 import pandas_datareader as pdr
 import pandas as pd
-import datetime
+from datetime import date
 
-local_Minimum_Date = datetime.datetime(2020, 3, 23)
+local_Minimum_Date = date(2020, 3, 23)
 
 print(local_Minimum_Date)
 
 with open('symbols') as symbolsFile:
     symbols = symbolsFile.read().splitlines()
 
+#2020.04.30, cey, Read in symbols, alphabetize, and remove duplicates
 print(symbols)
 
 os.environ["TIINGO_API_KEY"]="e64599a94ac46e01331bbe02499e7fd8cb7b8e84"
-df = pdr.get_data_tiingo('GOOG', api_key=os.getenv('TIINGO_API_KEY'))
-print(df.columns)
-print(df.index)
-print(df.head())
 
-print (df.loc[('GOOG', '2020-04-28 00:00:00+00:00')]);
+for symbol in symbols:
+	df = pdr.get_data_tiingo(symbol, local_Minimum_Date, date.today(), api_key=os.getenv('TIINGO_API_KEY'))
+	print(df.columns)
+	print(df.index)
+	print(df.head())
+
+#print (df.loc[('GOOG', '2020-04-28 00:00:00+00:00')]);
 
 #today_df=df[df['date']=='2020-04-28 00:00:00+00:00']
 #print(today_df.head())
