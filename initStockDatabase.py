53489205aaee4d056bb4ee0695f50186a7f6cfac
Chance Yohman <chance.yohman@gmail.com>
2020-05-05 07:33:28 -0400

    Closes #7

--- initStockDatabase.py
+++ initStockDatabase.py
@@ -1,2 +1,12 @@
 import sqlite3
 conn = sqlite3.connect('stocks.db')
+
+#conn.execute('''CREATE TABLE stocks
+#             (date text, symbol text, adjClose real)''')
+
+#conn.commit()
+
+for row in conn.execute('SELECT * FROM stocks'):
+        print(row)
+
+conn.close()
