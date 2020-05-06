4a6edd6d9bddc775fba62c7aa2b8e83750624795
Chance Yohman <chance.yohman@gmail.com>
2020-05-05 21:28:27 -0400

    Relates to #8

--- stockTrading.py
+++ stockTrading.py
@@ -4,6 +4,7 @@ import pandas as pd
 from datetime import datetime
 from datetime import date
 import csv
+import sqlite3
 
 local_Minimum_Date = date(2020, 3, 23)
 
@@ -20,31 +21,37 @@ symbols = list(dict.fromkeys(symbols))
 #2020.05.02, cey, Output the symbols to the original list
 os.rename('symbols', 'old_symbol_lists/symbols_'+datetime.now().strftime("%m-%d-%Y_%H:%M"))
 
-print(symbols)
-
 newSymbolsFile=open('symbols','w')
 
 for element in symbols:
-     print(element)
      newSymbolsFile.write(element)
      newSymbolsFile.write('\n')
 
 newSymbolsFile.close()
 
 #2020.04.30, cey, Read in symbols, alphabetize, and remove duplicates
-print(symbols)
 
 os.environ["TIINGO_API_KEY"]="e64599a94ac46e01331bbe02499e7fd8cb7b8e84"
 
+conn = sqlite3.connect('stocks.db')
+
 for symbol in symbols:
 	df = pdr.get_data_tiingo(symbol, local_Minimum_Date, date.today(), api_key=os.getenv('TIINGO_API_KEY'))
-	print(df.columns)
 	print(df.index)
-	print(df.head())
+	
+	i = 0	
 
-#print (df.loc[('GOOG', '2020-04-28 00:00:00+00:00')]);
+	for row in df.index:
+	       print(i)
+	       symbol = row[0]
+	       date = row[1].date()
+	       cur= conn.execute('SELECT COUNT(*) FROM stocks WHERE symbol=:symbol and date=:date', {"symbol": symbol, "date":  date})
+	       if (cur.fetchone()[0] > 0):
+	          print("Entry found")
+	       else:
+		  #print df[row]
+	          print("No entry found")
+	          #entry = [(date, symbol,
+                  #conn.execute('INSERT INTO stocks value(?,?,?)', 
 
-#today_df=df[df['date']=='2020-04-28 00:00:00+00:00']
-#print(today_df.head())
-
-#print(pdr.tiingo.get_tiingo_symbols())
+#2020.04.30, cey, Store data in database
